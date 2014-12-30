4f1fa03a5f1826b017493472d7cbbeb4700300af
HawkOwl <hawkowl@atleastfornow.net>
2014-12-30 21:37:13 +0800

    write a script to update it automatically maybe

---
+++ .travis/build_docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ "TRAVIS_PULL_REQUEST" == "false" ]] && [[ "TRAVIS_BRANCH" == "master" ]]; then
+
+    pip install pydoctor
+
+    REPO=`git config remote.origin.url`
+    REV=`git rev-parse HEAD`
+
+    git clone --branch gh-pages https://github.com/twisted/tubes.git /tmp/tmp-docs
+
+    pydoctor tubes
+
+    rsync -rt --del --exclude=".git" apidocs /tmp/tmp-docs/docs/
+
+    cd /tmp/tmp-docs
+
+    git config user.name "${GIT_USER}"
+    git config user.email "${GIT_EMAIL}"
+
+    git add -A
+    git commit -m "Built from ${REV}"
+
+    git push -q "https://${GH_TOKEN}@github.com/twisted/tubes.git"
+
+fi;

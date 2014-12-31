d966b219dd33f446a5e9ace23128ee82935d44a1
HawkOwl <hawkowl@atleastfornow.net>
2014-12-31 18:00:35 +0800

    improve a bit

--- .travis/build_docs.sh
+++ .travis/build_docs.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if [[ "TRAVIS_PULL_REQUEST" == "false" ]] && [[ "TRAVIS_BRANCH" == "master" ]]; then
+if [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_BRANCH} == "master" ]]; then
 
     echo "uploading docs"
 
-    pip install pydoctor
-
     REV=`git rev-parse HEAD`
 
+    # Build the docs
+    tox -e apidocs
+
+    # Make the directory
     git clone --branch gh-pages https://github.com/twisted/tubes.git /tmp/tmp-docs
 
-    pydoctor tubes
-
-    rsync -rt --del --exclude=".git" apidocs /tmp/tmp-docs/docs/
+    # Copy the docs
+    rsync -rt --del --exclude=".git" apidocs/* /tmp/tmp-docs/docs/
 
     cd /tmp/tmp-docs
 
+    # set the username and email
     git config user.name "${GIT_USER}"
     git config user.email "${GIT_EMAIL}"
 
     git add -A
     git commit -m "Built from ${REV}"
 
+    # Push it up
     git push -q "https://${GH_TOKEN}@github.com/twisted/tubes.git"
 else
     echo "skipping docs upload"

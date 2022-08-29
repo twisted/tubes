6e74afbb16d9adf0cc08b389e718376ea9c050b7
szpecek <55459908+szpecek@users.noreply.github.com>
2022-08-29 12:27:29 +0200

    Update classifiers in setup.py
    Udpate classifiers required to some pre checks verification like migration projects from ptyhon2.7 to python3

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
 from setuptools import setup
-setup()
+setup(
+  python_requires=">=3.6",
+  classifiers=[
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
